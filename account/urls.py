f806aa52512b51e1d15f0ed90039cbfe0326d349
Abhishek <apabhishek178@gmail.com>
2017-12-01 23:04:00 +0530

    done

--- account/urls.py
+++ account/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.contrib.auth.views import login, logout
 urlpatterns = [
     url(r'^$', views.home),
-    url(r'^login/$', login, {'template_name': 'account/login.html'}),
+    url(r'^login/$', login, {'template_name': 'account/Login.html'}),
     url(r'^logout/$', logout, {'template_name': 'account/logout.html'}),
     url(r'^register/$', views.register, name='register'),
     url(r'^profile/$', views.profile, name='view_profile'),
