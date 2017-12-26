ef832afe880a1e41aa2e07b12a8c91aa9b887cc6
Abhishek <apabhishek178@gmail.com>
2017-12-26 18:46:00 +0530

    done with controls'

--- tutorial/views.py
+++ tutorial/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 
 
 def login_redirect(request):
     return render(request, 'account/main_page.html')
 
 def controls(request):
-    return render(request, 'account/controls.html')
+    if request.method == 'POST':
+        return redirect('/account/')
+
+    else:
+       return render(request, 'account/controls.html')
